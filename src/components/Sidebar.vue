30fb993a94b00218e9587c943985161c3a35837c
bao <baobao@michealdeMacBook-Pro.local>
2018-05-25 19:45:33 +0800

    后台管理模板

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sidebar">
+    <nav class="sidebar-nav">
+      <sidebar-item :routes='menus'></sidebar-item>
+
+    </nav>
+  </div>
+</template>
+<script>
+import SidebarItem from './SidebarItem';
+import { mapGetters } from 'vuex';
+import menus from '../menu-config';
+
+export default {
+  name: 'sidebar',
+  data() {
+    return {
+      menus: menus
+    }
+  },
+  components: { SidebarItem },
+      computed: {
+        ...mapGetters([
+          'siderbar_routers'
+        ])
+      },
+  methods: {
+    handleClick (e) {
+      e.preventDefault()
+      e.target.parentElement.classList.toggle('open')
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .nav-link {
+    cursor:pointer;
+  }
+</style>
